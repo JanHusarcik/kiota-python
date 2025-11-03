8822a80c6158684f0a6b059991684c95d3f0bde5
jano_desktop <JanHusarcik@users.noreply.github.com>
2025-11-03 18:52:09 +0100

    Revert "fix: only use base_uri from http client"
    This reverts commit 9b0ec300fa765906465333211ba0bcee1af4b838.

--- packages/http/httpx/kiota_http/httpx_request_adapter.py
+++ packages/http/httpx/kiota_http/httpx_request_adapter.py
@@ -87,7 +87,9 @@ class HttpxRequestAdapter(RequestAdapter):
         if not http_client:
             http_client = KiotaClientFactory.create_with_default_middleware()
         self._http_client: httpx.AsyncClient = http_client
-        self._base_url: str = str(http_client.base_url) if http_client.base_url is not None else ""
+        if not base_url:
+            base_url = str(http_client.base_url) if http_client.base_url is not None else ""
+        self._base_url: str = base_url
         if not observability_options:
             observability_options = ObservabilityOptions()
         self.observability_options = observability_options
